f9b30d2cd984daec1b38b21dca06d317018239d6
David Brainer-Banker <dbrainer-banker@salesforce.com>
2017-03-16 14:39:45 -0600

    Add donate buttons and hr on mobile

--- src/Tile.js
+++ src/Tile.js
@@ -4,12 +4,17 @@ import Column from './Column';
 
 const Tile = ({ children, description, img, title }) => (
 	<Column size="one-half">
+		<hr className="u-hidden-on-desktop u-margin-top-x-large u-margin-bottom-large" />
 		<img
 			alt={title}
-			className="u-max-full-width"
+			className="u-max-full-width u-margin-top-x-large"
 			src={img}
 		/>
-		<p className="u-margin-top-small"><strong>{title}.</strong> {description || children}</p>
+		<div className="u-margin-top-small u-margin-bottom-medium">
+			<strong>{title}. </strong>
+			{description || children}
+		</div>
+		<a className="button button-primary" href="#">Donate</a>
 	</Column>
 );
 
