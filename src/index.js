e9079513f592665c4c4e30e81579e421927d5acd
David Brainer-Banker <dbrainer-banker@salesforce.com>
2017-04-11 11:05:07 -0600

    Scroll to top

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import Donate from './Donate';
 import Footer from './Footer';
 import Home from './Home';
 import NotFound from './NotFound';
+import ScrollToTop from './components/scroll-to-top';
 
 import './normalize.css';
 import './skeleton.css';
@@ -29,17 +30,19 @@ document.body.appendChild(script);
 
 ReactDOM.render(
   <Router>
-    <div className="u-margin-top-x-large">
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route path='/donate/:campaignKey' component={Donate}/>
-          <Route component={NotFound}/>
-        </Switch>
-        <Footer />
+    <ScrollToTop>
+      <div className="u-margin-top-x-large">
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route path='/donate/:campaignKey' component={Donate}/>
+            <Route component={NotFound}/>
+          </Switch>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </ScrollToTop>
   </Router>,
   document.getElementById('root')
 );
