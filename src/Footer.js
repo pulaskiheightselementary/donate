308e3fc772b0b97d8d68d0d816cf407c32d5e7e1
David Brainer-Banker <dbrainer-banker@salesforce.com>
2017-04-12 12:43:10 -0600

    Add a hosting message

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import Footer from './components/footer';
 
 const GlobalFooter = () => (
   <Footer>
-		<p>The Pulaski Heights Elementary PTA is a 501(c)(3) not-for-profit corporation. All donations through this site are tax-deductible.</p>
+		<p>The Pulaski Heights Elementary PTA is a 501(c)(3) not-for-profit corporation. All donations through this site are tax-deductible. Hosting by <a href="https://www.netlify.com/">Netlify</a></p>
 
 		<p>To help the greatest number of children and ensure that no gift is wasted, funds raised on this page will be used to support programs in the greatest imminent need. 100% of funds support the endeavors of the PHE PTA as we strive to make our school a great choice for all families.</p>
   </Footer>
