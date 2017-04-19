6aae5e0e3fc7f87a6b761fb046fc1a5f8b203467
David Brainer-Banker <dbrainer-banker@salesforce.com>
2017-04-18 19:08:11 -0600

    Update the images for a child in need

--- src/data.js
+++ src/data.js
@@ -5,6 +5,7 @@ import gardenProgramDonate from './images/garden-program-2.jpg';
 import teacherGrants from './images/teacher-grants.jpg';
 import teacherGrantsDonate from './images/teacher-grants-2.jpg';
 import schoolSupplies from './images/school-supplies.jpg';
+import schoolSuppliesDonate from './images/school-supplies-2.jpg';
 import artMusic from './images/art-music.jpg';
 import artMusicDonate from './images/art-music-2.jpg';
 import academicCompetitions from './images/academic-competitions.jpg';
@@ -33,7 +34,7 @@ const campaigns = shuffle([
 		title: 'Support a PHE Child in Need',
 		description: 'The PTA wants to make sure that every child can begin their day with a full stomach and come to school prepared with the necessary supplies to succeed. Money donated to this fund will provide meals and school supplies to children in need.',
 		img: schoolSupplies,
-		donateImg: schoolSupplies,
+		donateImg: schoolSuppliesDonate,
 		key: 'supplies',
 		embedCode: 'a-child-in-need'
 	},
